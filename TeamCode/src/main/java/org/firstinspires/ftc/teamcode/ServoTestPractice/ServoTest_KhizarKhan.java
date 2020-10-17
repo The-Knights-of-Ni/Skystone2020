3f5b4be005ebe74aad4d6db4296ad0a11f0f7edc
Rahizo <khankhizarm@gmail.com>
2020-10-17 18:23:38 -0400

    Khizar Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestPractice/ServoTest_KhizarKhan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestPractice/ServoTest_KhizarKhan.java
@@ -15,6 +15,7 @@ public class ServoTest_KhizarKhan extends LinearOpMode{
     double CurrentTime = 0;
     ElapsedTime timer = new ElapsedTime();
 
+
     public void initOpMode() throws IOException {
         this.robot = new Robot (this, timer);
 
@@ -31,9 +32,98 @@ public class ServoTest_KhizarKhan extends LinearOpMode{
         telemetry.clearAll();
 
         waitForStart(); //waits for start
-        //leftstickx, leftstickx2, leftsticky, leftsticky2, etc.
+
         while (opModeIsActive()) {
-            if (robot.leftStickX )
+            //Get gamepad inputs
+            robot.getGamePadInputs();
+
+            //Get the current time
+            CurrentTime = timer.nanoseconds();
+
+
+            //main claw arm servo test
+            if (robot.leftStickX > 0.5) //controls main claw ARM itself
+                robot.control.modifyServo(robot.mainClawArm, 0.005);
+            else if (robot.leftStickX > 0.1)
+                robot.control.modifyServo(robot.mainClawArm, 0.001);
+            else if (robot.leftStickX > -0.1) //do nothing
+                robot.control.modifyServo(robot.mainClawArm, -0.0);
+            else if  (robot.leftStickX > -0.5)
+                robot.control.modifyServo(robot.mainClawArm, -0.001);
+            else
+                robot.control.modifyServo(robot.mainClawArm, -0.005);
+
+            if (robot.rightStickY > 0.5) //these control main claw arm rotation
+                robot.control.modifyServo(robot.mainClawRotation, 0.005);
+            else if (robot.rightStickY > 0.1)
+                robot.control.modifyServo(robot.mainClawRotation, 0.001);
+            else if (robot.rightStickY > -0.1) //do nothing
+                robot.control.modifyServo(robot.mainClawRotation, -0.0);
+            else if  (robot.rightStickY > -0.5)
+                robot.control.modifyServo(robot.mainClawRotation, -0.001);
+            else
+                robot.control.modifyServo(robot.mainClawRotation, -0.005);
+
+            if (robot.rightStickX > 0.5) //controls main claw itself
+                robot.control.modifyServo(robot.mainClaw,0.005);
+            else if (robot.rightStickX > 0.1)
+                robot.control.modifyServo(robot.mainClaw,0.001);
+            else if (robot.rightStickX > -0.1)
+                robot.control.modifyServo(robot.mainClaw,0.0);
+            else if (robot.rightStickX > -0.5)
+                robot.control.modifyServo(robot.mainClaw,-0.001);
+            else
+                robot.control.modifyServo(robot.mainClaw,-0.005);
+
+        //EVERYTHING FROM HERE IS GAMEPAD 2
+            //controls servos for capstone arms
+            if (robot.leftStickX2 > 0.5)  //controls capstone arm
+                robot.control.modifyServo(robot.csArm,0.005);
+            else if (robot.leftStickX2 > 0.1)
+                robot.control.modifyServo(robot.csArm,0.001);
+            else if (robot.leftStickX2 > -0.1)
+                robot.control.modifyServo(robot.csArm,0.0);
+            else if (robot.leftStickX2 > -0.5)
+                robot.control.modifyServo(robot.csArm,-0.001);
+            else
+                robot.control.modifyServo(robot.csArm,-0.005);
+
+            if (robot.rightStickX2 > 0.5)  //controls capstone claw
+                robot.control.modifyServo(robot.csClaw,0.005);
+            else if (robot.rightStickX2 > 0.1)
+                robot.control.modifyServo(robot.csClaw,0.001);
+            else if (robot.rightStickX2 > -0.1)
+                robot.control.modifyServo(robot.csClaw,0.0);
+            else if (robot.rightStickX2 > -0.5)
+                robot.control.modifyServo(robot.csClaw,-0.001);
+            else
+                robot.control.modifyServo(robot.csClaw,-0.005);
+
+            //control servos for foundation claw
+            if (robot.leftStickY2 > 0.5)  //controls left foundation claw
+                robot.control.modifyServo(robot.fClawL,0.005);
+            else if (robot.leftStickY2 > 0.1)
+                robot.control.modifyServo(robot.fClawL,0.001);
+            else if (robot.leftStickY2 > -0.1)
+                robot.control.modifyServo(robot.fClawL,0.0);
+            else if (robot.leftStickY2 > -0.5)
+                robot.control.modifyServo(robot.fClawL,-0.001);
+            else
+                robot.control.modifyServo(robot.fClawL,-0.005);
+
+            if (robot.rightStickY2 > 0.5)  //controls right foundation claw
+                robot.control.modifyServo(robot.fClawR,0.005);
+            else if (robot.rightStickY2 > 0.1)
+                robot.control.modifyServo(robot.fClawR,0.001);
+            else if (robot.rightStickY2 > -0.1)
+                robot.control.modifyServo(robot.fClawR,0.0);
+            else if (robot.rightStickY2 > -0.5)
+                robot.control.modifyServo(robot.fClawR,-0.001);
+            else
+                robot.control.modifyServo(robot.fClawR,-0.005);
+
+            sleep(100);
+
         }
 
 
