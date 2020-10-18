badb86990aebffdbab767376b209b11f8af0325c
bhannah5 <kimhannah2005@gmail.com>
2020-10-17 20:04:26 -0400

    Hannah Kim Servo Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestPractice/ServoTest_HannahKim.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.ServoTestPractice;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.io.IOException;
+
+@TeleOp(name="Servo Test Hannah Kim")
+public class ServoTest_HannahKim extends LinearOpMode {
+
+    public void initOpMode() throws IOException{
+
+    }
+
+    public void runOpMode(){
+
+    }
+}
