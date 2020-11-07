eae7de89edb9aff893a4e0f2435ecd0ff07b7ad2
pwang1092 <plwdremington@gmail.com>
2020-11-07 15:44:28 -0800

    Update ServoTest_PeterWang.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestPractice/ServoTest_PeterWang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTestPractice/ServoTest_PeterWang.java
@@ -1,5 +1,18 @@
 package org.firstinspires.ftc.teamcode.ServoTestPractice;
 
-public class ServoTest_PeterWang {
-    
+import com.qualcomm.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.SubSystems.Robot;
+import java.io.IOException;
+
+public class ServoTest_PeterWang extends LinearOpMode {
+    private static final int targetPosition = 315;
+    private static final double maxPower = 0;
+    private static final float mmPerInch = 24.5f;
+
+    private Robot robot;
+
+    double timePre = 0;
+
+
 }
